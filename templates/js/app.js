196e2ccb177d52799a0d6508834bb97611e276c1
Martin Bruse <zondolfin@gmail.com>
2013-05-06 19:27:38 +0200

    working pan/zoom, but SO SLOW

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,2 +1,5 @@
 
+$(document).ready(function() {
+	$(".panzoom-elements").panzoom();
+});
 
--- web/controller.go
+++ web/controller.go
@@ -16,7 +16,10 @@ func allJs(w http.ResponseWriter, r *http.Request) {
 	}
 	data := getRequestData(w, r)
 	w.Header().Set("Content-Type", "application/javascript; charset=UTF-8")
+	renderText(w, r, jsTemplates, "jquery-2.0.0.min.js", data)
 	renderText(w, r, jsTemplates, "jquery.mobile-1.3.1.min.js", data)
+	renderText(w, r, jsTemplates, "modernizr.js", data)
+	renderText(w, r, jsTemplates, "jquery.panzoom.min.js", data)
 	renderText(w, r, jsTemplates, "app.js", data)
 }
 
