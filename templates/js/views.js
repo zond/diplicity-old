c4d2825e9f93d0f3c0ab4a23af65621e539ddb78
cmconnor <cmconnor@gmail.com>
2013-05-10 15:44:06 -0500

    Update views.js

--- templates/js/views.js
+++ templates/js/views.js
@@ -1,8 +1,10 @@
 $(function(){
   var UserPage=Backbone.View.extend({
   el1:$(".page"),
+  el2:$("#map"),
   render:function(){
     this.el1.html('hi there, the rendering worked');
+    this.el2.html('and this is a map');
   }
   });
   
