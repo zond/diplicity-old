b352c2eb7e033ba763710b344c9c81302b914f9d
cmconnor <cmconnor@gmail.com>
2013-05-10 17:04:38 -0400

    Update views.js

--- templates/js/views.js
+++ templates/js/views.js
@@ -1,16 +1,15 @@
 $(function(){
-  alert("hey");
+  alert("blah");
   var UserPage=Backbone.View.extend({
-    el1:$(".page"),
-    render:function(){
-      this.el1.html('hi there, the rendering worked');
-      alert("hi");
-    }
+  el:$(".page"),
+  render:function(){
+    alert("rendered");
+    this.el.html('hi there, the rendering worked');
+  }
   });
   
   var userPage=new UserPage();
   
   userPage.render();
-  });
-})
+});
   
