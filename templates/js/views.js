897b6d6bf1b4497a59db8be0564f6ddd0ce4ed2f
cmconnor <cmconnor@gmail.com>
2013-05-10 15:54:48 -0500

    Update views.js

--- templates/js/views.js
+++ templates/js/views.js
@@ -1,15 +1,18 @@
 $(function(){
   var UserPage=Backbone.View.extend({
-  el1:$(".page"),
-  el2:$("#map"),
-  render:function(){
-    this.el1.html('hi there, the rendering worked');
-    this.el2.html('and this is a map');
-  }
+    el1:$(".page"),
+    el2:$("#map"),
+    render:function(){
+      this.el1.html('hi there, the rendering worked');
+      $(document).ready ( function () {
+        this.el2.svg({loadURL: 'https://github.com/cmconnor/diplicity/blob/master/img/maps/standard.svg'});
+      });
+    }
   });
   
   var userPage=new UserPage();
   
   userPage.render();
+  });
 });
   
