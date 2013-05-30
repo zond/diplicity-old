4348efba324355e92f38f9605df0d1d5dfe2344e
Martin Bruse <zondolfin@gmail.com>
2013-05-30 11:09:06 +0200

    made the create_game view just render a game_member view instead of adding all the component parts of one.

--- templates/js/views/create_game.js
+++ templates/js/views/create_game.js
@@ -50,22 +50,10 @@ window.CreateGameView = BaseView.extend({
   render: function() {
 		var that = this;
 		that.$el.html(that.template({}));
-		_.each(variants(), function(variant) {
-		  if (variant.id == that.gameMember.get('game').variant) {
-				that.$('select.create-game-variant').append('<option value="{0}" selected="selected">{{.I "Variant"}}: {1}</option>'.format(variant.id, variant.name));
-			} else {
-				that.$('select.create-game-variant').append('<option value="{0}">{{.I "Variant"}}: {1}</option>'.format(variant.id, variant.name));
-			}
-		});
-		that.deadlines = {};
-		_.each(phaseTypes(that.$('.create-game-variant').val()), function(type) {
-		  that.$('.phase-types').append(new PhaseTypeView({
-				phaseType: type,
-				game: that.gameMember.get('game'),
-				owner: that.gameMember.get('owner'),
-				gameMember: that.gameMember,
-			}).doRender().el);
-		});
+		new GameMemberView({ 
+		  el: that.$('.create-game'),
+			model: that.gameMember,
+		}).doRender();
 		return that;
 	},
 
