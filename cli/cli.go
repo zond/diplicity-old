1779c74cac1622e60a253026ecd663f523c043bd
Martin Bruse <zondolfin@gmail.com>
2013-09-22 21:49:36 +0200

    started working on faking joins from the cli.

--- cli/cli.go
+++ cli/cli.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
+	"github.com/zond/diplicity/common"
+	"github.com/zond/diplicity/game"
 	"github.com/zond/diplicity/web"
+	"github.com/zond/kcwraps/subs"
 	"net/http"
 	"net/url"
 )
 
 func main() {
-	host := flag.String("host", "localhost", "The host to connect to")
-	port := flag.Int("port", 8080, "The port to connect to")
-	email := flag.String("email", "", "The email to fake authenticating as")
-	secret := flag.String("secret", web.DefaultSecret, "The cookie store secret of the server")
+	host := flag.String("host", "localhost", "The host to connect to.")
+	port := flag.Int("port", 8080, "The port to connect to.")
+	email := flag.String("email", "", "The email to fake authenticating as. Mandatory.")
+	secret := flag.String("secret", web.DefaultSecret, "The cookie store secret of the server.")
+	join := flag.String("join", "", "A game to join as the provided email.")
 
 	flag.Parse()
+
+	if *email == "" {
+		flag.Usage()
+		return
+	}
 
 	location, err := url.Parse(fmt.Sprintf("ws://%v:%v/ws", *host, *port))
 	if err != nil {
@@ -54,4 +64,32 @@ func main() {
 	if err != nil {
 		return
 	}
+
+	if *join != "" {
+		base64DecodedId, err := base64.StdEncoding.DecodeString(*join)
+		if err != nil {
+			panic(err)
+		}
+		if err := websocket.JSON.Send(ws, subs.Message{
+			Type: common.UpdateType,
+			Object: subs.Object{
+				URI: "/games/open",
+				Data: game.GameState{
+					Game: &game.Game{
+						Id: base64DecodedId,
+					},
+					Members: []game.MemberState{
+						game.MemberState{
+							Member: &game.Member{
+								PreferredNations: common.VariantMap[common.ClassicalString].Nations,
+							},
+						},
+					},
+				},
+			},
+		}); err != nil {
+			panic(err)
+		}
+	}
+
 }
