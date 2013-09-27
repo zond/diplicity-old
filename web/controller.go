acb26c295cdf4a842ebc6479da0750ec9c00f62a
Martin Bruse <zondolfin@gmail.com>
2013-09-27 16:10:06 +0200

    extracted wsBackbone from util

--- web/controller.go
+++ web/controller.go
@@ -190,6 +190,8 @@ func (self *Web) AllJs(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, ";")
 	self.renderText(w, r, self.jsTemplates, "panzoom.js", data)
 	fmt.Fprintln(w, ";")
+	self.renderText(w, r, self.jsTemplates, "wsBackbone.js", data)
+	fmt.Fprintln(w, ";")
 	self.render_Templates(data)
 	fmt.Fprintln(w, ";")
 	for _, templ := range self.jsModelTemplates.Templates() {
