46befe1c5bf9954160d119e469832c761ead64da
Martin Bruse <zondolfin@gmail.com>
2013-10-13 21:10:07 +0200

    added a working slider concept to show/hide the extra info inside a game (chat, orders, results)

--- templates/js/slider.js
+++ templates/js/slider.js
@@ -1,9 +1,13 @@
 $(window).load(function() {
-  $(document).on('click', '.slider .slider-control a', function(ev) {
+  $(document).on('click', '.slider-controls a', function(ev) {
 	  ev.preventDefault();
 		var link = $(ev.currentTarget);
 		var content = $('#' + link.attr('data-slider-id')); 
-		content.width(400);
-		content.removeClass('hidden');
+		if (content.hasClass('hidden')) {
+			content.width(400);
+			content.removeClass('hidden');
+		} else {
+		  content.addClass('hidden');
+		}
 	});
 });
