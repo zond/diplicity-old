8432ebbf7e5787a39555f58ebf036b943e06acdb
Martin Bruse <zondolfin@gmail.com>
2013-10-17 22:16:11 +0200

    made the play buttons nicer. made the sliders work better on small screens

--- templates/js/slider.js
+++ templates/js/slider.js
@@ -1,10 +1,25 @@
 $(window).load(function() {
+  $(window).on('orientationchange', function(ev) {
+	  $('.slider-content').addClass('hidden');
+	});
   $(document).on('click', '.slider-controls a', function(ev) {
 	  ev.preventDefault();
+		var singleMode = false;
+	  var windowWidth = $(window).width();
+		if (windowWidth < 1240) {
+		  singleMode = true;
+		}
 		var link = $(ev.currentTarget);
 		var content = $('#' + link.attr('data-slider-id')); 
 		if (content.hasClass('hidden')) {
-			content.width(400);
+		  if (singleMode) {
+			  $('.slider-content').addClass('hidden');
+			}
+			var width = $(window).width() - 40;
+			if (width > 400) {
+			  width = 400;
+			}
+			content.width(width);
 			content.removeClass('hidden');
 		} else {
 		  content.addClass('hidden');
