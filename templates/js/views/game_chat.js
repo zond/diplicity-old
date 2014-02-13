f4c9b1051fd4b092cce51c1197bcadd869d3f5a2
Martin Bruse <zondolfin@gmail.com>
2014-02-13 16:51:48 +0100

    more work on the create channel form

--- templates/js/views/game_chat.js
+++ templates/js/views/game_chat.js
@@ -2,12 +2,20 @@ window.GameChatView = BaseView.extend({
 
   template: _.template($('#game_chat_underscore').html()),
 
+	events: {
+	  "click .create-channel-button": "createChannel",
+	},
+
+	createChannel: function() {
+	  console.log('create channel!');
+	},
+
   render: function() {
 	  var that = this;
     that.$el.html(that.template({
 		}));
 		_.each(variantNations(that.model.get('Variant')), function(nation) {
-		  that.$('#new-channel-nations').append('<option value="' + nation + '">' + nation + '</option>');
+		  that.$('.new-channel-nations').append('<option value="' + nation + '">' + nation + '</option>');
 		});
 		that.$('.multiselect').multiselect({
 		  includeSelectAllOption: true,
