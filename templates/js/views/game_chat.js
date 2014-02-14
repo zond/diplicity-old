478ab97e1c1462cd7d70ff6497170fb92fd0065b
Martin Bruse <zondolfin@gmail.com>
2014-02-14 08:54:59 +0100

    more work on the chat channels

--- templates/js/views/game_chat.js
+++ templates/js/views/game_chat.js
@@ -34,20 +34,23 @@ window.GameChatView = BaseView.extend({
 	  var that = this;
     that.$el.html(that.template({
 		}));
-		_.each(variantNations(that.model.get('Variant')), function(nation) {
-		  that.$('.new-channel-nations').append('<option value="' + nation + '">' + nation + '</option>');
-		});
-		that.$('.multiselect').multiselect({
-		  includeSelectAllOption: true,
-			onDropdownHide: function(ev) {
-			  var el = $(ev.currentTarget);
-			  el.css('margin-bottom', 0);
-			},
-			onDropdownShow: function(ev) {
-			  var el = $(ev.currentTarget);
-			  el.css('margin-bottom', el.find('.multiselect-container').height());
-			},
-		});
+		var me = that.model.me();
+		if (me != null) {
+			_.each(variantNations(that.model.get('Variant')), function(nation) {
+				that.$('.new-channel-nations').append('<option ' + (nation == me.Nation ? 'selected="selected"=" ' : '') + 'value="' + nation + '">' + nation + '</option>');
+			});
+			that.$('.multiselect').multiselect({
+				includeSelectAllOption: true,
+				onDropdownHide: function(ev) {
+					var el = $(ev.currentTarget);
+					el.css('margin-bottom', 0);
+				},
+				onDropdownShow: function(ev) {
+					var el = $(ev.currentTarget);
+					el.css('margin-bottom', el.find('.multiselect-container').height());
+				},
+			});
+		}
 		return that;
 	},
 
