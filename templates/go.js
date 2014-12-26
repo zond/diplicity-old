16c7c21124ea6ce7c457de7f976f16972a12113b
Martin Bruse <zondolfin@gmail.com>
2014-12-26 23:14:16 +0100

    added more create game functionality

--- templates/go.js
+++ templates/go.js
@@ -1,6 +1,7 @@
 
 var GameStates = {{.GameStates}};
 var Variants = {{.Variants}};
+var OrderedVariants = {{.OrderedVariants}};
 var Consequences = {{.Consequences}};
 var ChatFlags = {{.ChatFlags}};
 var SecretFlags = {{.SecretFlags}};
